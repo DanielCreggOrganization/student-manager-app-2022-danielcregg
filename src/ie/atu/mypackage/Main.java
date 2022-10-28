f663216c15b82fa4bed8c86999d62932996374d5
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-10-28 12:44:11 +0000

    updates

--- src/ie/atu/mypackage/Main.java
+++ src/ie/atu/mypackage/Main.java
@@ -9,9 +9,10 @@ public class Main {
 
         studentManagerObject1.readStudentDataFromCSVFile("./resources/students.csv");
         studentManagerObject1.addStudentToList("G00666666", "Sarah", 29);
-        studentManagerObject1.showTotalStudents();
-        studentManagerObject1.printAllStudentDetails();
+        studentManagerObject1.printTotalNumberOfStudents();;
+        studentManagerObject1.printStudentList();
         studentManagerObject1.writeStudentDataToCSVFile("./resources/students.csv");
+        studentManagerObject1.getStudentList().contains(studentManagerObject1.findStudentByID("G00666666"));
 
         // userInput.close();
 
